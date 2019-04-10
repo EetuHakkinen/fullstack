eed008caa2483a0993e2c24a8e0268213878c744
Eetu Häkkinen <eetu.hakkinen@netti.fi>
2019-04-10 07:19:02 +0300

    4b

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -23,7 +23,4 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+module.exports = app;
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -5,7 +5,7 @@ blogsRouter.get('/', (request, response) => {
     Blog
         .find({})
         .then(blogs => {
-            response.json(blogs)
+            response.json(blogs.map(b => b.toJSON()))
         })
 })
 
@@ -17,6 +17,44 @@ blogsRouter.post('/', (request, response) => {
         .then(result => {
             response.status(201).json(result)
         })
+        .catch(() => {
+            response.status(400).end();
+        })
+});
+
+blogsRouter.delete('/:id', (request, response) => {
+    var id = request.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(() => {
+            response.status(200).end();
+        })
+        .catch(e => {
+            response.status(500).json(e);
+        });
+});
+
+blogsRouter.post('/:id', (req, res) => {
+    var id = req.params.id;
+    const body = req.body;
+
+    if (!body.likes) {
+        return res.status(400).json({ error: 'content missing'});
+    }
+
+    console.log(body);
+
+    const b = new Blog({
+        _id: id,
+        ...body
+    });
+
+    b.save().then(s => {
+        res.status(200).json(s.toJSON());
+    })
+    .catch(e => {
+        res.status(500).json(e);
+    })
+
 });
 
 module.exports = blogsRouter;
